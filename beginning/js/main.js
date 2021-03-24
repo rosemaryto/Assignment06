3d25c873f0fbe3dee69e9bcf5a837ef3e65a481b
rosemaryto <rosemary_to@ymail.com>
2021-03-23 20:28:47 -0700

    openPopup

--- beginning/js/main.js
+++ beginning/js/main.js
@@ -1,4 +1,12 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
+function init() {
+    "use strict";
+    var addEmp = window.document.getElementById("btnAddEmployee");
+    addEmp.addEventListener("click", function () {
+        window.open("add-employee.html", "btnAddEmployee", "width=400,height=400");    
+    });
+}
+window.addEventListener("load", init);
 
 // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
 
